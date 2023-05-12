8c3d196c3f13e16002aad808f5a252e0f3186d44
SayliMN <saylinarkhede@yahoo.com>
2023-05-12 00:11:47 -0700

    Changes in the projects and the styles.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,7 +115,7 @@ window.addEventListener('scroll',scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
-    const nav =document.getElementById('header')
+    const nav = document.getElementById('header')
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll',scrollHeader)
